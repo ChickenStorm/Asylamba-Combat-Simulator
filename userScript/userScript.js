86d19364c8ce72e24144749cce129c284d9a6b8f
ChickenStorm <tetras.aigle@yahoo.fr>
2015-01-06 17:25:41 +0100

    0.0.13

--- userScript/userScript.js
+++ userScript/userScript.js
@@ -24,28 +24,28 @@ function test(){
     for(var i in d){
         var regExp = new RegExp("^commander full show*")
         if (regExp.test(d[i].className)) {
-            alert(d[i].className)
+            
+            
+            d[i].onclick = function(event){
+                
+                
+                var temp = d[0]["outerHTML"]
+                
+                //var temp = ""
+                const SEARCH_TEXT = "data-army=\"["
+                
+                var pos = temp.indexOf(SEARCH_TEXT) + SEARCH_TEXT.length;
+                
+                temp = temp.substring(pos,temp.length-1);
+                
+               
+                var pos2 = temp.indexOf("]\">");
+                temp = temp.substring(0,pos2);
+                window.open("https://dl.dropboxusercontent.com/u/110049848/Projecet_script_public/Asylamba_project_online_launcher.html?d=" + temp +";");
+                
+               
+            };
         }
-        d[i].onclick = function(event){
-            
-            
-            var temp = d[0]["outerHTML"]
-            
-            //var temp = ""
-            const SEARCH_TEXT = "data-army=\"["
-            
-            var pos = temp.indexOf(SEARCH_TEXT) + SEARCH_TEXT.length;
-            
-            temp = temp.substring(pos,temp.length-1);
-            
-           
-            var pos2 = temp.indexOf("]\">");
-            temp = temp.substring(0,pos2);
-            window.open("https://dl.dropboxusercontent.com/u/110049848/Projecet_script_public/Asylamba_project_online_launcher.html?d=" + temp +";");
-            
-           
-        };
-        
     }
     
 }
